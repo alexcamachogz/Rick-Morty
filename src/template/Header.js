549d3c876aa835db7a634ec262f61038a20f28fa
Alejandra Camacho <camacho.ale94@gmail.com>
2020-08-14 15:20:25 -0500

    Add header template

---
+++ src/template/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+    const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <h1>
+                    <a href="/">100tifi.com</a>
+                </h1>
+            </div>
+            <div class="Header-nav">
+                <a href="#/about/">About</a>
+            </div>
+        </div>
+    `;
+
+    return view;
+}
+
+export default Header;
