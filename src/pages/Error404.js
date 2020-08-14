5852220ab6f1380a4c3a47c6198b80c99c186524
Alejandra Camacho <camacho.ale94@gmail.com>
2020-08-14 18:34:49 -0500

    Style Error404

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,7 +1,8 @@
 const Error404 = () => {
     const view = `
-        <div class="Error404">
-            <h2>Error 404</h2>
+        <div class="Error404 bg-red-400 w-screen mt-48 p-10">
+            <h2 class="text-center text-white text-3xl">Error 404</h2>
+            <p class="text-center text-white">Page Not Fount</p> 
         </div>
     `;
 
