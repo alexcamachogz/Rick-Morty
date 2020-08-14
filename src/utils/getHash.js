37c6ca53eba32528e5a7f65dfdd1841f10817586
Alejandra Camacho <camacho.ale94@gmail.com>
2020-08-14 16:02:09 -0500

    Implement and test connections

--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -1,4 +1,4 @@
-const getHash = () => {
-    return location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/';
-}
+const getHash = () =>
+    location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/'; //['','1','']
+
 export default getHash;
