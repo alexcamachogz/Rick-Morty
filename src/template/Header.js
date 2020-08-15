70a250280bf72f373209e01c82bd5e1fb018585a
Alejandra Camacho <camacho.ale94@gmail.com>
2020-08-14 22:42:52 -0500

    Last commit, last details

--- src/template/Header.js
+++ src/template/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/CientificoAPI">100tifi.com</a>
+                    <a href="/Rick-Morty">Rick & Morty</a>
                 </h1>
             </div>
             <div class="Header-nav">
